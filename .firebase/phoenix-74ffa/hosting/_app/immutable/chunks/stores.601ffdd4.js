42bfdba18fab9aeadf049c1324c497f14c237597
Jeremy <jeremyash13@gmail.com>
2023-12-09 21:03:33 -0700

    removing files from git repo that were mistakenly tracked by version control

--- .firebase/phoenix-74ffa/hosting/_app/immutable/chunks/stores.601ffdd4.js
+++
@@ -1,1 +0,0 @@
-import{w as a}from"./index.c59c70ef.js";const s=a(!1);export{s as i};
--- .firebase/phoenix-74ffa/hosting/scripts/site-redirect.js
+++
@@ -1,4 +0,0 @@
-if (window.location.hostname == "phoenix-74ffa.web.app" || 
-    window.location.hostname == "phoenix-74ffa.firebaseapp.com") {
-       window.location.href = `http://www.phoenixblocker.com${window.location.pathname}`; 
-}
